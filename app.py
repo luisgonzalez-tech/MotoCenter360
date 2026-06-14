c992f4246f08f019e59923fe496531cd621bc4ce
luisgonzalez-tech <luisangelgonzalez4677@gmail.com>
2026-06-14 12:01:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def catalogo():
 def taller():
     return render_template('taller.html')
 
+@app.route('/refacciones')
+def refacciones():
+    return render_template('refacciones.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
