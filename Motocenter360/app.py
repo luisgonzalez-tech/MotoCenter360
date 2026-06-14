eb7b11711624f47580794fbb6dc143090a412552
luisgonzalez-tech <luisangelgonzalez4677@gmail.com>
2026-06-13 18:26:12 -0600

    Add files via upload
    Actualización de estructura

--- Motocenter360/app.py
+++ Motocenter360/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Esta es nuestra "Base de datos" temporal en memoria
 inventario = {
     "Aceite": 15,
     "Balatas": 10,
@@ -12,6 +11,3 @@ inventario = {
 @app.route('/')
 def index():
     return render_template('index.html', stock=inventario)
-
-if __name__ == '__main__':
-    app.run(debug=True)
